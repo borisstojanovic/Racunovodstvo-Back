be60a2c4726ddfa5042403054b69c2383b9c5573
boris.stojanovic <boris.stojanovic@msg-global.com>
2022-05-08 15:46:38 +0200

    feature/nabavka: Sklonio dependency iz nabavke na knjizenje-service

--- nabavka/src/main/java/raf/si/racunovodstvo/nabavka/controllers/KonverzijaRestController.java
+++ nabavka/src/main/java/raf/si/racunovodstvo/nabavka/controllers/KonverzijaRestController.java
@@ -9,8 +9,8 @@ import org.springframework.http.*;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
-import raf.si.racunovodstvo.knjizenje.model.Preduzece;
-import raf.si.racunovodstvo.knjizenje.utils.SearchUtil;
+import raf.si.racunovodstvo.nabavka.responses.PreduzeceResponse;
+import raf.si.racunovodstvo.nabavka.utils.SearchUtil;
 
 import raf.si.racunovodstvo.nabavka.model.Konverzija;
 import raf.si.racunovodstvo.nabavka.requests.KonverzijaRequest;
@@ -21,6 +21,7 @@ import raf.si.racunovodstvo.nabavka.services.IKonverzijaService;
 import javax.persistence.EntityNotFoundException;
 import javax.persistence.PersistenceException;
 import javax.validation.Valid;
+
 import java.io.IOException;
 import java.util.Optional;
 
@@ -33,11 +34,11 @@ public class KonverzijaRestController {
 
     private final IKonverzijaService iKonverzijaService;
 
-    private RestTemplate restTemplate;
+    private final RestTemplate restTemplate;
 
     private final SearchUtil<Konverzija> searchUtil;
 
-    private String URL = "http://preduzece/api/preduzece/%d";
+    private static final String URL = "http://preduzece/api/preduzece/%d";
 
     public KonverzijaRestController(KonverzijaService iKonverzijaService, RestTemplate restTemplate) {
         this.iKonverzijaService = iKonverzijaService;
@@ -45,8 +46,8 @@ public class KonverzijaRestController {
         this.searchUtil = new SearchUtil<>();
     }
 
-    private Preduzece getPreduzeceById(Long id, String token) throws IOException {
-        if(id == null){
+    private PreduzeceResponse getPreduzeceById(Long id, String token) throws IOException {
+        if (id == null) {
             return null;
         }
         HttpHeaders headers = new HttpHeaders();
@@ -57,28 +58,30 @@ public class KonverzijaRestController {
         ResponseEntity<String> response = restTemplate.exchange(String.format(URL, id), HttpMethod.GET, request, String.class);
         String result = response.getBody();
         ObjectMapper objectMapper = new ObjectMapper();
-        return objectMapper.readValue(result, Preduzece.class);
+        return objectMapper.readValue(result, PreduzeceResponse.class);
     }
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> search(@RequestParam(name = "search") String search, @RequestHeader(name="Authorization") String token) throws IOException {
+    public ResponseEntity<?> search(@RequestParam(name = "search") String search, @RequestHeader(name = "Authorization") String token)
+        throws IOException {
         Specification<Konverzija> spec = this.searchUtil.getSpec(search);
         Page<KonverzijaResponse> result = iKonverzijaService.findAll(spec);
         return ResponseEntity.ok(result);
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> createKonverzija(@Valid @RequestBody KonverzijaRequest konverzija, @RequestHeader(name="Authorization") String token) throws IOException {
-        if(getPreduzeceById(konverzija.getDobavljacId(), token)== null){
-            throw new PersistenceException(String.format("Ne postoji dobavljac sa id-jem %s",konverzija.getDobavljacId()));
+    public ResponseEntity<?> createKonverzija(@Valid @RequestBody KonverzijaRequest konverzija,
+                                              @RequestHeader(name = "Authorization") String token) throws IOException {
+        if (getPreduzeceById(konverzija.getDobavljacId(), token) == null) {
+            throw new PersistenceException(String.format("Ne postoji dobavljac sa id-jem %s", konverzija.getDobavljacId()));
         }
         return ResponseEntity.ok(iKonverzijaService.saveKonverzija(konverzija));
     }
 
     @DeleteMapping(value = "/{id}")
-    public ResponseEntity<?> deleteKonverzija(@PathVariable("id") Long id){
+    public ResponseEntity<?> deleteKonverzija(@PathVariable("id") Long id) {
         Optional<Konverzija> optionalKonverzija = iKonverzijaService.findById(id);
-        if (optionalKonverzija.isPresent()){
+        if (optionalKonverzija.isPresent()) {
             iKonverzijaService.deleteById(id);
             return ResponseEntity.noContent().build();
         }
