f13c9c2d3dc38d29d52abf5c03d68fe024b70332
boris.stojanovic <boris.stojanovic@msg-global.com>
2022-07-03 01:28:33 +0200

    IGNORE

--- user/src/test/java/raf/si/racunovodstvo/user/integration/BaseIT.java
+++ user/src/test/java/raf/si/racunovodstvo/user/integration/BaseIT.java
@@ -3,6 +3,7 @@ package raf.si.racunovodstvo.user.integration;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.containers.wait.strategy.HostPortWaitStrategy;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import raf.si.racunovodstvo.user.integration.containers.MySQLMasterContainer;
@@ -15,23 +16,19 @@ class BaseIT {
 
     protected static final RedisContainer redisContainer = new RedisContainer(NetworkHolder.network(), 6379);
     protected static final MySQLMasterContainer mySQLMasterContainer = new MySQLMasterContainer(NetworkHolder.network(), 3306);
+    protected static final MySQLContainer mysqlContainer = new MySQLContainer("mysql:8.0.28");
 
     static {
-        redisContainer.withReuse(true);
-        mySQLMasterContainer.withReuse(true);
+        mysqlContainer.withReuse(true);
 
-        mySQLMasterContainer.start();
-        redisContainer.start();
-
-        HostPortWaitStrategy mysqlMasterWait = new HostPortWaitStrategy();
-        mysqlMasterWait.waitUntilReady(mySQLMasterContainer);
-        redisContainer.waitingFor(mysqlMasterWait);
+        mysqlContainer.start();
     }
 
     @DynamicPropertySource
     public static void properties(DynamicPropertyRegistry registry) {
         String mysqlHost =
-            "jdbc:mysql://" + mySQLMasterContainer.getHost() + ":" + mySQLMasterContainer.getMappedPort(3306) + "/si?serverTimezone=UTC";
+            "jdbc:mysql://" + mysqlContainer.getHost() + ":" + mysqlContainer.getMappedPort(3306) + "/" + mysqlContainer.getDatabaseName()
+                + "?serverTimezone=UTC";
 
         registry.add("spring.datasource.url", () -> mysqlHost);
         registry.add("spring.datasource.password", () -> "test");
