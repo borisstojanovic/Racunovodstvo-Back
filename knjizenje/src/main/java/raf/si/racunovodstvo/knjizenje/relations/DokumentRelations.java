888e958ab7aba20aad6d5d6a94e078f6b2e80208
boris.stojanovic <boris.stojanovic@msg-global.com>
2022-07-03 11:01:30 +0200

    bugfix/filter-dnevnik: Dodao filtriranje po broju dokumenta

--- knjizenje/src/main/java/raf/si/racunovodstvo/knjizenje/relations/DokumentRelations.java
+++ knjizenje/src/main/java/raf/si/racunovodstvo/knjizenje/relations/DokumentRelations.java
@@ -1,11 +1,35 @@
 package raf.si.racunovodstvo.knjizenje.relations;
 
 import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.Expression;
+import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
-public class DokumentRelations<T> extends ForeignKeyRelations<T>{
+import static java.lang.Long.parseLong;
+
+public class DokumentRelations<T> extends RacunRelations<T>{
+
+    protected Expression<String> brojDokumentaExpression;
+    protected final String brojDokumenta;
+
     public DokumentRelations(Root<T> root, CriteriaBuilder builder, String key, String val) {
         super(root, builder, key, val);
-        idExpression = root.get(key).get("dokumentId").as(Long.class);
+        brojDokumentaExpression = root.get(key).as(String.class);
+        brojDokumenta = val;
+    }
+
+    @Override
+    public Predicate greaterThanOrEqualTo() {
+        return this.builder.greaterThanOrEqualTo(brojDokumentaExpression,brojDokumenta);
+    }
+
+    @Override
+    public Predicate lessThanOrEqualTo() {
+        return this.builder.lessThanOrEqualTo(brojDokumentaExpression,brojDokumenta);
+    }
+
+    @Override
+    public Predicate equalTo() {
+        return this.builder.equal(brojDokumentaExpression,brojDokumenta);
     }
 }
