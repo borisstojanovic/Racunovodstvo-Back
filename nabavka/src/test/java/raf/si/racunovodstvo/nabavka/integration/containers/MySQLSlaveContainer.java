d7c1b680c825f1ca17758de55e69f03743568f16
boris.stojanovic <boris.stojanovic@msg-global.com>
2022-07-03 16:36:08 +0200

    bugfix/integration-testing: Ispravke za nabavke

--- nabavka/src/test/java/raf/si/racunovodstvo/nabavka/integration/containers/MySQLSlaveContainer.java
+++ nabavka/src/test/java/raf/si/racunovodstvo/nabavka/integration/containers/MySQLSlaveContainer.java
@@ -10,7 +10,7 @@ public class MySQLSlaveContainer extends MySQLContainer {
     public MySQLSlaveContainer(Network network, int port, String hostName) {
         super(network, port, hostName);;
 
-        withStartupCheckStrategy(new MinimumDurationRunningStartupCheckStrategy(Duration.ofMillis(18000)));
+        withStartupCheckStrategy(new MinimumDurationRunningStartupCheckStrategy(Duration.ofMillis(20000)));
         addEnv("MYSQL_REPLICATION_MODE", "slave");
         addEnv("MYSQL_ROOT_PASSWORD", "test");
         addEnv("MYSQL_MASTER_HOST", "mysql_db");
